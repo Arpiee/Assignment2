750d2701579ad8b0da5cd89cfd85e1e367fb9b7d
Arpiee <arpit.kohli01@gmail.com>
2017-12-15 16:32:11 -0500

    first commit

---
+++ Controllers/C2arpitphonesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class C2arpitphonesController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: C2arpitphones
+        public ActionResult Index()
+        {
+            return View(db.C2arpitphones.ToList());
+        }
+
+        // GET: C2arpitphones/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            C2arpitphones c2arpitphones = db.C2arpitphones.Find(id);
+            if (c2arpitphones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c2arpitphones);
+        }
+
+        // GET: C2arpitphones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: C2arpitphones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Phone,s_no,Condition,Rating")] C2arpitphones c2arpitphones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.C2arpitphones.Add(c2arpitphones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(c2arpitphones);
+        }
+
+        // GET: C2arpitphones/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            C2arpitphones c2arpitphones = db.C2arpitphones.Find(id);
+            if (c2arpitphones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c2arpitphones);
+        }
+
+        // POST: C2arpitphones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Phone,s_no,Condition,Rating")] C2arpitphones c2arpitphones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(c2arpitphones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(c2arpitphones);
+        }
+
+        // GET: C2arpitphones/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            C2arpitphones c2arpitphones = db.C2arpitphones.Find(id);
+            if (c2arpitphones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(c2arpitphones);
+        }
+
+        // POST: C2arpitphones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            C2arpitphones c2arpitphones = db.C2arpitphones.Find(id);
+            db.C2arpitphones.Remove(c2arpitphones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/arpitphonesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{[Authorize(Roles = "Asministrator")]
+    public class arpitphonesController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: arpitphones
+        public ActionResult Index()
+        {
+            return View(db.arpitphones.ToList());
+        }
+
+        [AllowAnonymous]
+        // GET: arpitphones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            arpitphone arpitphone = db.arpitphones.Find(id);
+            if (arpitphone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(arpitphone);
+        }
+
+        // GET: arpitphones/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: arpitphones/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "s_no,Phone,models,Price")] arpitphone arpitphone)
+        {
+            if (ModelState.IsValid)
+            {
+                db.arpitphones.Add(arpitphone);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(arpitphone);
+        }
+
+        // GET: arpitphones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            arpitphone arpitphone = db.arpitphones.Find(id);
+            if (arpitphone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(arpitphone);
+        }
+
+        // POST: arpitphones/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "s_no,Phone,models,Price")] arpitphone arpitphone)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(arpitphone).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(arpitphone);
+        }
+
+        // GET: arpitphones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            arpitphone arpitphone = db.arpitphones.Find(id);
+            if (arpitphone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(arpitphone);
+        }
+
+        // POST: arpitphones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            arpitphone arpitphone = db.arpitphones.Find(id);
+            db.arpitphones.Remove(arpitphone);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
